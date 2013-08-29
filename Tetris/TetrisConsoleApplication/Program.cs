acdc7ccd1c8c4b9b63f1eaa5a3e505fdb6619216
Aneliya Petkova <AneliyaP.Petkova@gmail.com>
2013-08-29 21:14:24 +0300

    Created Tetris.sln

---
+++ Tetris/TetrisConsoleApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisConsoleApplication
+{
+    class Program
+    {
+        static Engine GetEngineInstance()
+        {
+            return new Engine();
+        }
+
+        static void Main(string[] args)
+        {
+            Engine engine = GetEngineInstance();
+        }
+    }
+}
